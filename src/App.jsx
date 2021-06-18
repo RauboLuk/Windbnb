d65ceb2537595231f2c733cdfd3aa48c4a6b5f03
RauboLuk <rauboluk@gmail.com>
2021-06-19 00:09:34 +0200

    feat: property card html concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import data from "./assets/data/stays.json";
 import Header from "./components/Header";
+import PropertyCard from "./components/PropertyCard";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <PropertyCard location={data[0]} />
     </div>
   );
 };
--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,9 +1,29 @@
-const PropertyCard = () => {
-  return (
-    <div>
-      Card
-    </div>
-  )
-}
+import Pluralize from "pluralize";
+import StarRateIcon from "@material-ui/icons/StarRate";
 
-export default PropertyCard
+const PropertyCard = ({ location }) => {
+  return (
+    <section>
+      <img
+        src={location.photo}
+        alt={location.title}
+        data-testid="img"
+        className="rounded-3xl"
+      />
+      <section>
+        {location.superHost && <div data-testid="superHost">SUPER HOST</div>}
+        <div>
+          {location.type}
+          {location.beds && <> . {Pluralize("bed", location.beds, true)}</>}
+        </div>
+        <div>
+          {location.rating}
+          <StarRateIcon data-testid="starIcon" className="text-red-default" />
+        </div>
+      </section>
+      <section>{location.title}</section>
+    </section>
+  );
+};
+
+export default PropertyCard;
