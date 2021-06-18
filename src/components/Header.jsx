44c639317765d2286d36f23b45c492f52d3fd29b
RauboLuk <rauboluk@gmail.com>
2021-06-18 20:54:16 +0200

    feat: header with inputs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,32 @@ import SearchIcon from "@material-ui/icons/Search";
 
 const Header = ({ openSearch }) => {
   return (
-    <header>
-      <img src={Logo} alt="logo" />
+    <header className="font-mulish font-normal text-sm grid gap-5 px-5">
+      <img src={Logo} alt="logo" className="my-5" />
       <section
         onClick={openSearch}
         data-testid="searchBar"
         style={{
           boxShadow: "0px 1px 6px rgba(0, 0, 0, 0.1)",
         }}
+        className="min-w-min mx-5 h-14 flex flex-row items-center rounded-2xl"
       >
-        <input type="text" placeholder="Choose location" />
-        <SearchIcon
-          style={{
-            color: "#EB5757",
-          }}
-          data-testid="searchIcon"
+        <input
+          type="text"
+          placeholder="Choose location"
+          className="w-6/12 px-3 outline-none placeholder-gray-light text-gray-darkest"
         />
-        <input type="text" placeholder="Add guests" />
+        <div className="h-full border-r border-white-milk"></div>
+        <input
+          type="text"
+          placeholder="Add guests"
+          className="w-5/12 px-3 outline-none placeholder-gray-light text-gray-darkest"
+        />
+        <div className="h-full border-r border-white-milk"></div>
+        <SearchIcon
+          data-testid="searchIcon"
+          className="w-min m-4 text-red-default"
+        />
       </section>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          default: "#EB5757",
+        },
+        white: {
+          default: "#FFFFFF",
+          milk: "#F2F2F2",
+        },
+        gray: {
+          darkest: "#333333",
+          dark: "#4F4F4F",
+          light: "#BDBDBD",
+          g3: "#828282",
+        },
+      },
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
