c663d5d2602d75341fe769fa9ea0355db11519bc
RauboLuk <rauboluk@gmail.com>
2021-06-28 23:05:28 +0200

    test: test-utils added

---
+++ src/test-utils.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import guestsReducer from "./slices/guestsSlice";
+import locationReducer from "./slices/locationSlice";
+import staysReducer from "./slices/staysSlice";
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: {
+        guests: guestsReducer,
+        location: locationReducer,
+        stays: staysReducer,
+      },
+    }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from "@testing-library/react";
+// override render method
+export { render };
