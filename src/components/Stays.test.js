e6360fc2a0990362b738da93d4ad95b384633a55
RauboLuk <rauboluk@gmail.com>
2021-06-29 23:50:08 +0200

    test: stays tests fix

--- src/components/Stays.test.js
+++ src/components/Stays.test.js
@@ -2,19 +2,33 @@ import { render, screen } from "../test-utils";
 
 import Stays from "./Stays";
 
+const location = { city: "xd", country: "tes" };
+
 describe("Stays", () => {
   test("there is a info about country", () => {
-    render(<Stays />);
-    const propertyElement = screen.getByText("Stays in Finland");
+    render(<Stays />, {
+      preloadedState: {
+        location,
+      },
+    });
+    const propertyElement = screen.getByText(`Stays in ${location.country}`);
     expect(propertyElement).toBeInTheDocument();
   });
   test("there is a info about stays count", () => {
-    render(<Stays />);
+    render(<Stays />, {
+      preloadedState: {
+        stays: [location, location],
+      },
+    });
     const propertyElement = screen.getByText("2 stays");
     expect(propertyElement).toBeInTheDocument();
   });
   test("there is a correct singular form for 1 stay", () => {
-    render(<Stays />);
+    render(<Stays />, {
+      preloadedState: {
+        stays: [location],
+      },
+    });
     const propertyElement = screen.getByText("1 stay");
     expect(propertyElement).toBeInTheDocument();
   });
